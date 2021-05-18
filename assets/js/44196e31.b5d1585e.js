1bd9cbd69d90c7b3178f3ba8db10665533a98f9a
RobLukas <r.lukasik@clearcode.cc>
2021-05-18 14:31:29 +0200

    add config deployment

--- assets/js/44196e31.b5d1585e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[176],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/gh-page-deployment/blog/welcome"},{"title":"Hello","permalink":"/gh-page-deployment/blog/hello-world"},{"title":"Hola","permalink":"/gh-page-deployment/blog/hola"}]}')}}]);
